7e0dc835788c9f5cc4258dc58887f2a74225f8e2
Hristo <hristo@obg.bg>
2025-02-13 22:34:47 +0200

    Update Crowdfunding.sol

--- 05-solidity-fundamentals/Crowdfunding.sol
+++ 05-solidity-fundamentals/Crowdfunding.sol
@@ -1,55 +1,47 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.27;
 
 struct Vote {
     address shareholder;
     uint256 shares;
-    //uint256 timestamp;
+    uint256 timestamp;
 }
+
+error InsuficientAmount();
 
 contract Crowdfunding {
     mapping(address => uint256) public shares;
     Vote[] public votes;
     uint256 public a;
+    uint256 sharePrice;
+    uint256 totalShares;
 
-    function addShares(address receiver) external {
-        address b;
-        uint256 c;
-        uint256 a = 5;
-        // totalShares += amount;
-        shares[receiver] += 1000;
+    constructor(uint256 _initialSharePrice) {
+        sharePrice = _initialSharePrice; //set the initial share price
+    }
+
+
+    function addShares() external payable {
+        uint256 sharesToReceive = msg.value / sharePrice;
+        totalShares += sharesToReceive;
+        shares[msg.sender] += msg.value;
+
+        if(msg.value < sharePrice) {
+            revert InsuficientAmount();
+        }
+
+        if(msg.value % sharePrice > 0) {
+            revert InsuficientAmount();
+        }
     }
 
     function vote(address holder) external {
         votes.push(
             Vote({
                 shareholder: holder,
-                shares: shares[holder]
-                //timestamp: block.timestamp
+                shares: shares[holder],
+                timestamp: block.timestamp
             })
         );
-    }
-}
-
-contract Test {
-    uint256[5] public arr = [5, 3];
-
-    uint256[] public dynamicArr = [1, 2, 3, 4, 5];
-
-    function addNumberDynamic(uint256 value) external returns(uint256) {
-        dynamicArr.push(value);
-
-        return dynamicArr.length;
-    }
-
-    function addNumber() external view returns (uint256) {
-        uint256 res;
-
-        for (uint256 i = 0; i < arr.length; i++) {
-            res += arr[i];
-        }
-
-        return res;
     }
 }
